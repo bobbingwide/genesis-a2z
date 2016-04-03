8c9c3209e3387e699e88506cdb5dce76802a084e
Herb Miller <herb@bobbingwide.com>
2016-04-03 12:02:04 +0100

    First draft at automatically creating the 'All post_type' menu items

--- archive.php
+++ archive.php
@@ -2,14 +2,77 @@
 
 /**
  * Until today 2015/12/17 this file contained _gob__FILE__();
- I must hve been trying to find out how the archive template was being displayed.
+ I must have been trying to find out how the archive template was being displayed.
  */
  
 //remove_action( "genesis_footer_widgets", 
 remove_action( "genesis_entry_content", "genesis_do_post_content" );
-remove_action( "genesis_entry_footer", 'genesis_oik_post_info' );
-remove_action( "genesis_entry_footer", "genesis_entry_footer_markup_open", 5);
-remove_action( "genesis_entry_footer", "genesis_entry_footer_markup_close", 15 );
+
+
+/**
+ * <!--
+action genesis_entry_content genesis_loop,genesis_entry_content
+
+: 8   genesis_do_post_image;1
+: 10   genesis_do_post_content;1
+: 12   genesis_do_post_content_nav;1
+: 14   genesis_do_post_permalink;1--> 
+*/
+
+
+//add_action( "genesis_entry_content", "genesis_do_post_image", 4 );
+remove_action( "genesis_entry_content", "genesis_do_post_content", 10 );
+remove_action( "genesis_entry_content", "genesis_do_post_content_nav", 12 ); 
+ 
+// Not necessary to remove if we don't invoke the action
+ 
+//remove_action( "genesis_entry_footer", 'genesis_oik_post_info' );
+//remove_action( "genesis_entry_footer", "genesis_entry_footer_markup_open", 5);
+//remove_action( "genesis_entry_footer", "genesis_entry_footer_markup_close", 15 );
+
+//remove_action( "genesis_entry_header", "genesis_do_post_format_image", 4 );
+//remove_action( "genesis_before_entry_conten", "genesis
+
+remove_action( "genesis_loop", "genesis_do_loop" );
+add_action( "genesis_loop", "genesis_a2z_do_loop" );
+
+//add_action( "genesis_a2z_entry_content", "genesis_a2z_entry_content" );
+
+//function genesis_a2z_entry_content() {
+//
+//}
+
+
+/**
+ * Implement a tighter loop for archives
+ * 
+ * Basically we don't want any content except the featured image
+ * 
+ * BUT 
+ * one day we might look at {@link https://github.com/desandro/masonry}
+ * 
+ */
+function genesis_a2z_do_loop() {
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post();
+			do_action( 'genesis_before_entry' );
+			printf( '<article %s>', genesis_attr( 'entry' ) );
+			//do_action( 'genesis_before_entry_content' );
+			//printf( '<div %s>', genesis_attr( 'entry-content' ) );
+			do_action( 'genesis_entry_header' );
+			do_action( 'genesis_entry_content' );
+			//echo '</div>';
+			//do_action( 'genesis_after_entry_content' );
+			//do_action( 'genesis_entry_footer' );
+			echo '</article>';
+			do_action( 'genesis_after_entry' );
+		}
+		do_action( 'genesis_after_endwhile' );
+	} else {
+		do_action( 'genesis_loop_else' );
+	}
+}
 
 
 
---
+++ create-all-post-type-menu-items.php
@@ -0,0 +1,63 @@
+<?php // (C) Copyrigght Bobbing Wide 2016
+
+function genesis_a2z_create_all_post_type_menu_items() {
+
+	$term_id = get_primary_menu(); 
+	
+	$post_types = bw_as_array( "oik-plugins,oik-themes,oik_shortcodes,oik_api,oik_file,oik_hook,oik_class,oik_request" );
+	
+	foreach ( $post_types as $post_type ) {
+		$post_type_object = get_post_type_object( $post_type );
+		$plural_label = $post_type_object->labels->name;
+		
+		echo "$post_type, $plural_label" . PHP_EOL;
+		//print_r( $post_type_object );
+		if ( false !== strpos( $plural_label, "oik" ) ) {
+			list( $oik, $plural_label ) = explode( " ", $plural_label );
+			$plural_label = ucfirst( $plural_label );
+		}
+		echo "$post_type, $plural_label" . PHP_EOL;
+		
+		genesis_a2z_insert_post_type_archive_menu_item( $term_id, $post_type, $plural_label ); 
+		//gob();
+		
+																																							
+	}
+}
+
+/**
+ * Return the ID of the primary menu
+ *
+ * We could pass this in as an ID! 
+ * 
+ * 
+ * For core.wp.a2z it's 2
+ * For qw/oikcom it's ?
+ * For qw/wordpress we want the a2z menu which is 321
+ * 
+ */
+
+function get_primary_menu( $name="Primary" ) {
+	$nav_menus = wp_get_nav_menus();
+	//print_r( $nav_menus );
+	
+
+	return( 321 );
+}
+	
+	
+function genesis_a2z_insert_post_type_archive_menu_item( $term_id, $post_type, $plural_label ) {
+  $menu_item_data = array( 'menu-item-type' => 'post_type_archive'
+                         //, 'menu-item-object-id' => 
+                         , 'menu-item-object' => $post_type
+                         , 'menu-item-title' => $plural_label
+                         , 'menu-item-status' => 'publish'
+                         );
+  $menu_item_db_id = wp_update_nav_menu_item( $term_id, 0, $menu_item_data );
+	bw_trace2( $menu_item_db_id, "menu_item" );
+  return( $menu_item_db_id);
+}
+
+genesis_a2z_create_all_post_type_menu_items();  
+	
+	
