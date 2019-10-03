718881e9213f5722b0207b9520c803a3591b087a
Herb Miller <herb@bobbingwide.com>
2019-10-03 21:33:06 +0100

    Issue #19 - Use a full colour palette which includes the 11 default colours, plus Vivid Purple, White and the Link and Accent colours set by the user in the Customizer

--- config/appearance.php
+++ config/appearance.php
@@ -26,6 +26,73 @@ $genesis_a2z_accent_color = get_theme_mod(
 $genesis_a2z_link_color_contrast   = genesis_a2z_color_contrast( $genesis_a2z_link_color );
 $genesis_a2z_link_color_brightness = genesis_a2z_color_brightness( $genesis_a2z_link_color, 35 );
 
+$genesis_a2z_default_palette = array(
+	array(
+		'name' => __( 'Pale pink', 'genesis-a2z' ),
+		'slug' => 'pale-pink',
+		'color' => '#f78da7',
+	),
+	array(
+		'name' => __( 'Vivid red', 'genesis-a2z' ),
+		'slug' => 'vivid-red',
+		'color' => '#cf2e2e',
+	),
+	array(
+		'name' => __( 'Luminous vivid orange', 'genesis-a2z' ),
+		'slug' => 'luminous-vivid-orange',
+		'color' => '#ff6900',
+	),
+	array(
+		'name' => __( 'Luminous vivid amber', 'genesis-a2z' ),
+		'slug' => 'luminous-vivid-amber',
+		'color' => '#fcb900',
+	),
+	array(
+		'name' => __( 'Light green cyan', 'genesis-a2z' ),
+		'slug' => 'light-green-cyan',
+		'color' => '#7bdcb5',
+	),
+	array(
+		'name' => __( 'Vivid green cyan', 'genesis-a2z' ),
+		'slug' => 'vivid-green-cyan',
+		'color' => '#00d084',
+	),
+	array(
+		'name' => __( 'Pale cyan blue', 'genesis-a2z' ),
+		'slug' => 'pale-cyan-blue',
+		'color' => '#8ed1fc',
+	),
+	array(
+		'name' => __( 'Vivid cyan blue', 'genesis-a2z' ),
+		'slug' => 'vivid-cyan-blue',
+		'color' => '#0693e3',
+	),
+	array(
+		'name' => __( 'Vivid purple', 'genesis-a2z' ),
+		'slug' => 'vivid-purple',
+		'color' => '#9b51e0',
+	),
+	array(
+		'name' => __( 'Very light gray', 'genesis-a2z' ),
+		'slug' => 'very-light-gray',
+		'color' => '#eeeeee',
+	),
+	array(
+		'name' => __( 'Cyan bluish gray', 'genesis-a2z' ),
+		'slug' => 'cyan-bluish-gray',
+		'color' => '#abb8c3',
+	),
+	array(
+		'name' => __( 'Very dark gray', 'genesis-a2z' ),
+		'slug' => 'very-dark-gray',
+		'color' => '#313131',
+	),
+	array(
+		'name' => __( 'White', 'genesis-a2z' ),
+	    'slug' => 'white',
+	    'color' => '#ffffff', )
+);
+
 return array(
 	'fonts-url'            => 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,700&display=swap',
 	'content-width'        => 1062,
@@ -34,36 +101,36 @@ return array(
 	'button-outline-hover' => $genesis_a2z_link_color_brightness,
 	'link-color'           => $genesis_a2z_link_color,
 	'default-colors'       => $genesis_a2z_default_colors,
-	'editor-color-palette' => array(
+	'editor-color-palette' => array_merge( $genesis_a2z_default_palette, 	array(
 		array(
-			'name'  => __( 'Custom color', 'genesis-sample' ), // Called “Link Color” in the Customizer options. Renamed because “Link Color” implies it can only be used for links.
+			'name'  => __( 'Link colour', 'genesis-a2z' ),
 			'slug'  => 'theme-primary',
 			'color' => $genesis_a2z_link_color,
 		),
 		array(
-			'name'  => __( 'Accent color', 'genesis-sample' ),
+			'name'  => __( 'Accent colour', 'genesis-a2z' ),
 			'slug'  => 'theme-secondary',
 			'color' => $genesis_a2z_accent_color,
 		),
-	),
+	)),
 	'editor-font-sizes'    => array(
 		array(
-			'name' => __( 'Small', 'genesis-sample' ),
+			'name' => __( 'Small', 'genesis-a2z' ),
 			'size' => 12,
 			'slug' => 'small',
 		),
 		array(
-			'name' => __( 'Normal', 'genesis-sample' ),
+			'name' => __( 'Normal', 'genesis-a2z' ),
 			'size' => 18,
 			'slug' => 'normal',
 		),
 		array(
-			'name' => __( 'Large', 'genesis-sample' ),
+			'name' => __( 'Large', 'genesis-a2z' ),
 			'size' => 20,
 			'slug' => 'large',
 		),
 		array(
-			'name' => __( 'Larger', 'genesis-sample' ),
+			'name' => __( 'Larger', 'genesis-a2z' ),
 			'size' => 24,
 			'slug' => 'larger',
 		),
